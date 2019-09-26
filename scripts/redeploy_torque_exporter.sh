b5327c7e4226e9dc1a8f184a70edb64e6ff525de
euxdat <none@the.moment>
2019-09-26 09:21:12 +0000

    fixed path

--- scripts/redeploy_torque_exporter.sh
+++ scripts/redeploy_torque_exporter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo -i kubectl delete deployments. torque-exporter
-sudo -i kubectl create -f /home/linux/ATOSES_spiros/GITHUB-torque_exporter/torque_exporter/yaml/torque_exporter.yaml
+sudo -i kubectl create -f /home/linux/ATOSES_spiros/GITHUB-ari-apc-lab/torque_exporter/yaml/torque_exporter.yaml
 
